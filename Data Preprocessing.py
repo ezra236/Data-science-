e25118e4f4e965ff8e004ab423b0f6786d83c0c1
EZRA <125749134+ezra236@users.noreply.github.com>
2024-06-09 21:20:40 +0300

    Transforming to numerals

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -1,24 +1,20 @@
 import pandas as pd
-from IPython.display import display
 
-# Load the CSV file into a DataFrame
-input_csv_file = "team_statistics.csv"
-df = pd.read_csv(input_csv_file)
 
-# Data preprocessing
-# Convert percentage strings to numerical values
-df['PossessionAccuracy'] = df['PossessionAccuracy'].str.rstrip('%').astype('float') / 100
-df['PassingAccuracy'] = df['PassingAccuracy'].str.rstrip('%').astype('float') / 100
+path="team_statistics.csv"
+df = pd.read_csv(path)
 
-# Check for missing values
-missing_values = df.isnull().sum()
-print("Missing values in each column:\n", missing_values)
+i=10
 
-# Display the DataFrame
-display(df)
+for column in df.columns:
+    if pd.api.types.is_numeric_dtype(df[column]):
+        print(f"Column Name:{column}\n")
+        print(f"Data Types:{df[column].dtype}")
+        print(f"Descriptive Statistics: {df[column].describe()}\n")
+    else:
+        if 'PossessionAccuracy' in df.columns:
+           df['PossessionAccuracy'] = df['PossessionAccuracy'].str.rstrip('%').astype('float') / 100.0
+        if 'PassingAccuracy' in df.columns:
+           df['PassingAccuracy'] = df['PassingAccuracy'].str.rstrip('%').astype('float') / 100.0
 
-# Save the preprocessed DataFrame to a new CSV file
-output_csv_file = "preprocessed_team_statistics.csv"
-df.to_csv(output_csv_file, index=False)
-
-print(f"Data successfully written to {output_csv_file}")
+          
