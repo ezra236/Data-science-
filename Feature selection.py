d79cd4344c9b8be5fc46448b2ce4322267cde967
EZRA <125749134+ezra236@users.noreply.github.com>
2024-06-11 20:52:17 +0300

    Feature selection assuming the target variable is win and using correlation method

--- Feature selection.py
+++ Feature selection.py
@@ -1,29 +1,25 @@
 import pandas as pd
 from scipy.stats import pearsonr, spearmanr
+import seaborn as sns
+import matplotlib.pyplot as plt 
 
-# Creating the DataFrame
-data = {
-    'Matches': [8, 8, 10, 8, 8, 8, 8],
-    'Win': [6, 7, 10, 4, 7, 6, 7],
-    'Drawn': [2, 1, 0, 2, 0, 1, 0],
-    'Lost': [0, 0, 0, 2, 1, 0, 1],
-    'Goals': [22, 29, 36, 16, 25, 22, 30],
-    'Team Name': ['England', 'France', 'Portugal', 'Italy', 'Spain', 'Belgium', 'Germany'],
-    'Goals conceded': [4, 3, 2, 9, 5, 4, 7],
-    'PossessionAccuracy': ['62.63%', '60.13%', '63.10%', '58.80%', '67.30%', '58.13%', '60%'],
-    'Balls Recovered': [273, 297, 422, 321, 276, 258, 299],
-    'Clean Sheets': [4, 6, 9, 3, 4, 5, 5],
-    'Saves': [5, 15, 16, 15, 11, 16, 14],
-    'Yellow cards': [14, 12, 11, 14, 16, 13, 12],
-    'Red Cards': [1, 0, 0, 0, 0, 1, 0],
-    'PassingAccuracy': ['80%', '90%', '89%', '88%', '90%', '86%', '89%']
-}
-df = pd.DataFrame(data)
 
-# Convert percentage strings to float
-df['PossessionAccuracy'] = df['PossessionAccuracy'].str.rstrip('%').astype(float)
-df['PassingAccuracy'] = df['PassingAccuracy'].str.rstrip('%').astype(float)
+path="team_statistics.csv"
+df = pd.read_csv(path)
 
+
+for column in df.columns:
+    if pd.api.types.is_numeric_dtype(df[column]):
+        print(f"Column Name:{column}\n")
+        print(f"Data Types:{df[column].dtype}")
+        print(f"Descriptive Statistics: {df[column].describe()}\n")
+    else:
+        if 'PossessionAccuracy' in df.columns:
+           df['PossessionAccuracy'] = df['PossessionAccuracy'].str.rstrip('%').astype('float') / 100.0
+        if 'PassingAccuracy' in df.columns:
+           df['PassingAccuracy'] = df['PassingAccuracy'].str.rstrip('%').astype('float') / 100.0
+
+print(df)          
 # Dropping 'Team Name' as it's categorical and non-numeric
 df.drop('Team Name', axis=1, inplace=True)
 
